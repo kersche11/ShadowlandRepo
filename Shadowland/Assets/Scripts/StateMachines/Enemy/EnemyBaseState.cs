047adb2c760d7a3dbab2da9139cfc76d6f6fc014
kersche11 <kerschbaumer.gerald@gmail.com>
2023-03-26 17:12:42 +0200

    Add EnemyBaseState and Statemachine

---
+++ Shadowland/Assets/Scripts/StateMachines/Enemy/EnemyBaseState.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyBaseState : State
+{
+    protected EnemyStateMachine stateMachine;
+
+
+    public EnemyBaseState(EnemyStateMachine stateMachine) 
+    {
+        this.stateMachine = stateMachine;
+    } 
+   
+
+   
+}
---
+++ Shadowland/Assets/Scripts/StateMachines/Enemy/EnemyIdleState.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdleState : EnemyBaseState
+{
+    private readonly int LocomotionBlendTreeHash = Animator.StringToHash("Locomotion");
+
+    private readonly int speedHash = Animator.StringToHash("Speed");
+
+    private const float CrossFadeDuration = 0.1f;
+    private const float AnimatorDampTime = 0.1f;
+    public EnemyIdleState(EnemyStateMachine stateMachine) : base(stateMachine){}
+
+    public override void Enter()
+    {
+        stateMachine.Animator.CrossFadeInFixedTime(LocomotionBlendTreeHash,CrossFadeDuration);
+     
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        //Setze Speed des Enemies smooth auf 0 (Idle)
+        stateMachine.Animator.SetFloat(speedHash, 0f, AnimatorDampTime,deltaTime);
+    }
+
+    public override void Exit()
+    {
+        
+    }
+
+    
+}
